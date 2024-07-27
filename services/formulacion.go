f9bcbdbc8df2ad5d65189d03d78f162de4b31c7f
Steven <st2ven.cabarcas@gmail.com>
2024-07-26 19:26:21 -0500

    feat: actuallización del issue 1114

--- services/formulacion.go
+++ services/formulacion.go
@@ -1580,10 +1580,34 @@ func GetVinculacionTerceroByEmail(terceroEmail string) (interface{}, error) {
 	}
 }
 
+func obtenerCorreoPlaneacion() (string, error) {
+	var respuestaPeticion map[string]interface{}
+	var correoPlaneacion map[string]interface{}
+	baseURL := "http://" + beego.AppConfig.String("ParametrosService") + "/parametro_periodo?query="
+	err := request.GetJson(baseURL+"ParametroId.CodigoAbreviacion:CORREO_OAP,ParametroId.TipoParametroId.CodigoAbreviacion:P_SISGPLAN,Activo:true", &respuestaPeticion)
+	jsonData, ok := respuestaPeticion["Data"].([]interface{})[0].(map[string]interface{})["Valor"].(string)
+
+	if err != nil || !ok {
+		return "", fmt.Errorf("no se pudo obtener el correo de la Oficina de Asesora de Planeación del módulo de parámetros, comuníquese con computo@udistrital.edu.co")
+	}
+	err1 := json.Unmarshal([]byte(jsonData), &correoPlaneacion)
+	if err1 != nil {
+		return "", fmt.Errorf("Error al deserializar el JSON de Correo Oficina Planeacion: ", err)
+	}
+	return correoPlaneacion["Valor"].(string), nil
+}
+
 func CambioCargoIdVinculacionTercero(idVinculacion string, cuerpo []byte) (*models.Vinculacion, error) {
+	const ROL_ASISTENTE_PLANEACION = "ASISTENTE_PLANEACION"
+	var vinculacionPlaneacion, rolAsistentePlaneacion bool
 	var vinculacion []models.Vinculacion
 	var body map[string]interface{}
 	json.Unmarshal(cuerpo, &body)
+
+	correoPlaneacion, errorPeticion := obtenerCorreoPlaneacion()
+	if errorPeticion != nil {
+		panic(map[string]interface{}{"funcion": "CambioCargoIdVinculacionTercero", "err": errorPeticion.Error(), "status": "400", "log": errorPeticion})
+	}
 
 	idNoRegistra, idJefeOficina, idAsistenteDependencia, err := formulacionhelper.ObtenerIdParametros()
 	if err != nil {
@@ -1593,6 +1617,23 @@ func CambioCargoIdVinculacionTercero(idVinculacion string, cuerpo []byte) (*mode
 	err = request.GetJson("http://"+beego.AppConfig.String("TercerosService")+"/vinculacion?query=Activo:true,Id:"+idVinculacion, &vinculacion)
 	if err != nil || vinculacion[0].CargoId == 0 {
 		return nil, errors.New("error del servicio CambioCargoIdVinculacionTercero: Error al obtener vinculacion" + err.Error())
+	}
+
+	vinculaciones := body["user"].(map[string]interface{})["Vinculacion"].([]interface{})
+	vinculacionSeleccionada := body["user"].(map[string]interface{})["VinculacionSeleccionadaId"]
+
+	for _, vinculacion := range vinculaciones {
+		if vinculacion.(map[string]interface{})["Id"].(float64) == vinculacionSeleccionada {
+			if DependenciaCorreo, ok := vinculacion.(map[string]interface{})["DependenciaCorreo"].(string); ok {
+				vinculacionPlaneacion = DependenciaCorreo == correoPlaneacion
+				rolAsistentePlaneacion = (body["rol"].(string) == ROL_ASISTENTE_PLANEACION)
+				if rolAsistentePlaneacion && !vinculacionPlaneacion && body["vincular"] == true {
+					return nil, fmt.Errorf("El usuario no puede tener el rol de %s si no pertenece a la Oficina de Asesora de Planeación", ROL_ASISTENTE_PLANEACION)
+				}
+			} else {
+				return nil, fmt.Errorf("no se pudo obtener la Dependencia asociada a la vinculación")
+			}
+		}
 	}
 
 	if vinculacion[0].CargoId == int(idJefeOficina) || vinculacion[0].CargoId == int(idAsistenteDependencia) || vinculacion[0].CargoId == int(idNoRegistra) {
